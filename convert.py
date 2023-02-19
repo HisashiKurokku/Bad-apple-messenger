1b84198edd0d4d1dda5e1e17e0ad8e33deb20166
Kurokku <114159361+HisashiKurokku@users.noreply.github.com>
2023-02-19 15:48:46 +0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,27 +1,43 @@
 import cv2
+import json
 import os
 
-video_path = " "#<-- Video path here
+print('Processing... ')
+print('This should take at least 15 seconds')
 
-frames_dir = " "#<-- Frames directory here
-if not os.path.exists(frames_dir):
-    os.makedirs(frames_dir)
+video_path = "<video path here>"
+output_file_path = "<output file path here>"
+os.makedirs(os.path.dirname(output_file_path), exist_ok=True)
 
 video = cv2.VideoCapture(video_path)
 
-fps = 1
+fps = 2
 
 frame_count = 0
 
+ascii_frames = []
+
 while True:
-    
+
     ret, frame = video.read()
 
     if not ret:
         break
 
-    frame_filename = os.path.join(frames_dir, f"{frame_count:04d}.jpg")
-    cv2.imwrite(frame_filename, frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    resized_gray = cv2.resize(gray, (80, 60), interpolation=cv2.INTER_AREA)
+
+    ascii_frame = ""
+    for row in resized_gray:
+        for pixel in row:
+            if pixel < 128:
+                ascii_frame += "o"
+            else:
+                ascii_frame += " "
+        ascii_frame += "\n"
+
+    ascii_frames.append(ascii_frame)
 
     frame_count += 1
 
@@ -29,3 +45,6 @@ while True:
 
 video.release()
 cv2.destroyAllWindows()
+
+with open(output_file_path, "w", encoding="utf-8") as output_file:
+    json.dump(ascii_frames, output_file, ensure_ascii=False)
